87095b355c055a2dacfa06b742b1e7694f0545f8
yangming <yangming@cbim.com.cn>
2023-04-03 17:20:20 +0800

    init

---
+++ 09_show_chinese/test_charset_utf8_copy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <wchar.h>
+
+int main(int argc, char **argv)
+{
+	wchar_t c = L'中';
+	unsigned char* charP = (unsigned char *)&c;
+	int i;
+	
+	printf("Hex code: ");
+	for (i = 0; i < sizeof(wchar_t); i++)
+	{
+		printf("%02x ", charP[i]);
+	}
+	printf("\n");
+	return 0;
+}
